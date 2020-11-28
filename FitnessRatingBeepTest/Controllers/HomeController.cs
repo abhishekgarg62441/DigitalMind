4cccd30e26330c4df600d12a6251f32fef6fb47a
Abhishek kumar <Abhishekgarg62441@gmail.com>
2020-11-28 12:15:39 +0530

    initialCommit

---
+++ FitnessRatingBeepTest/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FitnessRatingBeepTest.Models;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace FitnessRatingBeepTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Athlete> athletes = JsonConvert.DeserializeObject<List<Athlete>>(json);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FitnessRatingBeepTest/Models/Athlete.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessRatingBeepTest.Models
+{
+    public class Athlete
+    {
+        public string AtheleteID { get; set; }
+        public string AtheleteName { get; set; }
+        public string IsWarned { get; set; }
+        public string IsStoped { get; set; }
+        public string SpeedLevel { get; set; }
+        public string ShuttleNo { get; set; }
+    }
+}
